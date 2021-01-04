66d14b40acd9ac9a4bc2892216932b47b1496abb
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2021-01-04 18:00:54 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+    const [data, setData] = useState(null);
+    const onClick = async () => {
+        try {
+            const response = await axios.get(
+                'http://newsapi.org/v2/top-headlines?country=kr&apiKey=132ac4ad06874aceac3a10219365e84e',
+                );
+            setData(response.data);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    return (
+        <div>
+            <div>
+                <button onClick={onClick}>불러오기</button>
+            </div>
+            {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
+        </div>
+    );
+};
+
+export default App;

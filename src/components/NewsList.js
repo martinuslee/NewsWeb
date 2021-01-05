9b7f04f425b7d437feef5ee0f3a4ec04f6b0983c
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2021-01-05 16:05:38 +0900

    usePromise

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -16,37 +17,26 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-    const [ articles, setArticles ] = useState(null);
-    const [ loading, setLoading ] = useState(false);
-
-    useEffect(() => {
-        // async를 사용하는 함수 따로 선언
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const response = await axios.get(
-                    `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=132ac4ad06874aceac3a10219365e84e`,
-                );
-                setArticles(response.data.articles);
-                console.log(query);
-            }catch (e) {
-                console.log(e);
-            }
-            setLoading(false);
-        };
-        fetchData();
-    }, [ category ]);
+    const [loading , response, error] = usePromise(()=>{
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(
+            `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=132ac4ad06874aceac3a10219365e84e`,
+        );
+    }, [category]);
 
     // 대기 중일 때
     if (loading) {
         return <NewsListBlock>대기중...</NewsListBlock>;
     }
     // 아직 articles 값이 설정되지 않았을 때
-    if (!articles) {
+    if (!response) {
         return null;
     }
+    if(error){
+        return <NewsItemBlock>에러 발생!</NewsItemBlock>;
+    }
     //articles 값이 유효할 때
+    const { articles } = response.data;
     return (
         <NewsListBlock>
             {articles.map(article => (
